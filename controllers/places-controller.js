738bde2fb36ebf81e2cdaa1151901ba610f56aee
DareDefyMe <70864245+DareDefyMe@users.noreply.github.com>
2021-03-20 22:32:45 +0500

    refactoring

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,3 +1,19 @@
+const HttpError = require('../models/http-errors');
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1' ,
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!!!',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 W 34th St, New York, NY 10001',
+        creator: 'u1'
+    }
+];
+
 const getPlaceById = (req, res, next) => {
     const placeId = req.params.pid;
     const place = DUMMY_PLACES.find(p => {
@@ -22,3 +38,6 @@ const getPlaceByUserId = (req, res, next) => {
     }
     return res.json({place});
 };
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,46 +1,10 @@
 const { json } = require('body-parser');
 const express = require('express');
-const HttpError = require('../models/http-errors');
-
+const placeController = require('../controllers/places-controller');
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1' ,
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!!!',
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: '20 W 34th St, New York, NY 10001',
-        creator: 'u1'
-    }
-];
+router.get('/:pid', placeController.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId;
-    });
-
-    if(!place) {
-        throw new HttpError("Could not find the place for the provided property", 404);
-    }
-
-    console.log('GET Request in Places');
-    return res.json({place});
-});
-
-router.get('/users/:uid', (req, res, next) => {
-    const userId = req.params.uid;
-    const place = DUMMY_PLACES.find(u => {
-        return u.creator === userId;
-    });
-    if(!place) {
-        return next(new HttpError("Could not find the place for the provided property", 404));
-    }
-    return res.json({place});
-});
+router.get('/users/:uid', placeController.getPlaceByUserId);
 
 module.exports = router;
