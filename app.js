be84272ff1777f01fd83d4b4cb42bd4c735b65d8
DareDefyMe <70864245+DareDefyMe@users.noreply.github.com>
2021-03-21 22:42:03 +0500

    unsupported routes handler middleware

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const placesRoutes = require("./routes/places-routes");
-
+const HttpError = require("./models/http-errors");
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this url', 404);
+    throw error;
+});
 
 app.use((error, req, res, next) => {
     if(res.headerSent) {
