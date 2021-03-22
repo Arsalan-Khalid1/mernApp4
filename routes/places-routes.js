6b91a47e444953e61866f8dd2af180158bab7746
DareDefyMe <70864245+DareDefyMe@users.noreply.github.com>
2021-03-22 18:33:20 +0500

    createPlace API input validation

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const placeController = require('../controllers/places-controller');
+const {check} = require('express-validator');
 const router = express.Router();
 
 router.get('/:pid', placeController.getPlaceById);
 
 router.get('/users/:uid', placeController.getPlacesByUserId);
 
-router.post('/', placeController.createPlace);
+router.post('/',
+[
+check('title').not().isEmpty(),
+check('description').isLength({min:5}),
+check('coordinates').not().isEmpty()
+], 
+placeController.createPlace);
 
 router.patch('/:pid', placeController.updatePlace);
 
