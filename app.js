161b063eb2b0b058cd7e6b10c2aaf583d66668bb
DareDefyMe <70864245+DareDefyMe@users.noreply.github.com>
2021-04-21 01:24:26 +0500

    fixes

--- app.js
+++ app.js
@@ -5,12 +5,30 @@ const mongoose = require("mongoose");
 const HttpError = require("./models/http-errors");
 const app = express();
 
-app.use(express.json());
+const port = 5000;
 
+app.use(express.json());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Accesss-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Cotent-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
+//   next();
+// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,DELETE,POST,PATCH");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  next();
+});
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
-
-const port = 5000;
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this url", 404);
@@ -28,7 +46,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://DDM:myplacebookdb@cluster0.vewb8.mongodb.net/PlaceBook?retryWrites=true&w=majority",
+    "mongodb+srv://DDM:myplacebookdb@cluster0.vewb8.mongodb.net/mern?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const placeController = require('../controllers/places-controller');
-const {check} = require('express-validator');
+const express = require("express");
+const placeController = require("../controllers/places-controller");
+const { check } = require("express-validator");
 const router = express.Router();
 
-router.get('/:pid', placeController.getPlaceById);
+router.get("/:pid", placeController.getPlaceById);
 
-router.get('/users/:uid', placeController.getPlacesByUserId);
+router.get("/user/:uid", placeController.getPlacesByUserId);
 
-router.post('/',
-[
-check('title').not().isEmpty(),
-check('description').isLength({min:5}),
-check('address').not().isEmpty()
-], 
-placeController.createPlace);
+router.post(
+  "/",
+  [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("address").not().isEmpty(),
+  ],
+  placeController.createPlace
+);
 
-router.patch('/:pid', 
-[
-check('title').not().isEmpty(),
-check('description').isLength({min:5})
-],
-placeController.updatePlace);
+router.patch(
+  "/:pid",
+  [check("title").not().isEmpty(), check("description").isLength({ min: 5 })],
+  placeController.updatePlace
+);
 
-router.delete('/:pid', placeController.deletePlace);
+router.delete("/:pid", placeController.deletePlace);
 
 module.exports = router;
